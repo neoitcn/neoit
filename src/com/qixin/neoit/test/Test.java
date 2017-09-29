5749716b9ec31d725ab9e6b788bdfaef1a40f940
zhuyuanjie123 <30727042+zhuyuanjie123@users.noreply.github.com>
2017-09-29 09:11:54 +0800

    测试
    测试

--- src/com/qixin/neoit/test/Test.java
+++ src/com/qixin/neoit/test/Test.java
@@ -2,6 +2,7 @@ package com.qixin.neoit.test;
 
 import java.io.File;
 import java.io.InputStream;
+import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -30,16 +31,53 @@ import com.qixin.neoit.biz.IndexBiz;
 import com.qixin.neoit.entity.Edu_news;
 import com.qixin.neoit.entity.Sys_user;
 
+import sun.misc.BASE64Encoder;
+import sun.security.provider.MD5;
+
 public class Test {
 
 	public static void main(String[] args) throws Exception {
 	
-		List<Sys_user> createrList=new ArrayList<Sys_user>();
+	/*	List<Sys_user> createrList=new ArrayList<Sys_user>();
 		Sys_user user=new Sys_user();
 		user.setRealName("323");
 		createrList.add(user);
-		System.out.println("createrList:"+createrList.get(0).getRealName());
+		System.out.println("createrList:"+createrList.get(0).getRealName());*/
+	
+	
+	    //String	message="123211";//MD5:65F3772CD5D16F190CE4991408114607
+	    String	message="zhuyuanjie11";///MD5:5EA4021F17007A563956FFC563A475E1
+	    String	md5="";
+		MessageDigest md = MessageDigest.getInstance("MD5");  // 创建一个md5算法对象  
+		byte[] messageByte = message.getBytes("UTF-8");  
+		byte[] md5Byte = md.digest(messageByte);              // 获得MD5字节数组,16*8=128位  
+	    md5 = bytesToHex(md5Byte);       
+	
+	   System.out.println("MD5:"+md5);
+		
+		/*String str="我是你爸爸";
+	   //确定计算方法
+       MessageDigest md5=MessageDigest.getInstance("MD5");
+       BASE64Encoder base64en = new BASE64Encoder();
+       //加密后的字符串
+       String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
+       System.out.println("newstr:"+newstr);*/
 	
 	}
-
+	
+	public static String bytesToHex(byte[] bytes) {  
+	        StringBuffer hexStr = new StringBuffer();  
+	        int num;  
+	        for (int i = 0; i < bytes.length; i++) {  
+	            num = bytes[i];  
+	             if(num < 0) {  
+	                 num += 256;  
+	            }  
+	            if(num < 16){  
+	                hexStr.append("0");  
+	            }  
+	            hexStr.append(Integer.toHexString(num));  
+	        }  
+	        return hexStr.toString().toUpperCase();  
+	}  
 }
